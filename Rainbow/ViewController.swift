c8a5ac11b6d908983b3ce3d8bba33019bced1a13
Abneed <memoboy262@gmail.com>
2019-04-03 08:05:58 -0500

    Initial Commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Guillermo Abneed Rodriguez Velazquez on 4/3/19.
+//  Copyright © 2019 Guillermo Abneed Rodriguez Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
